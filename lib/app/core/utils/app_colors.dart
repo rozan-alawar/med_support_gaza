f907d17b2d96b0e35ff884c50a091f2d39583da2
SajaHerez <herezsaja2020@gmail.com>
2024-12-23 13:46:58 +0200

    add images and custom fonts to project assets

--- lib/app/core/utils/app_colors.dart
+++ lib/app/core/utils/app_colors.dart
@@ -9,14 +9,13 @@ class AppColors {
 
 // ألوان النصوص
   static const Color textDark = Color(0xFF1F1F1F); // أسود فاتح
-  static const Color textLight = Color(0xFF757575); // رمادي
+  static const Color textLight = Color(0xFF72777A); // رمادي
   static const Color white = Colors.white;
   static const Color transparent = Colors.transparent;
 
 // ألوان إضافية
   static const Color error = Color(0xFFB00020); // أحمر داكن
-  static const Color success = Color(0xFF4CAF50); // أخضر
+  static const Color success = Color(0xFF00918E); // أخضر
   static const Color warning = Color(0xFFFFC107); // أصفر
   static const Color textField = Color(0xFFF7F7F);
-
 }
