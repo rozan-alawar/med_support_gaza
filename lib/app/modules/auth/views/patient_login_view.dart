630330634ab5da88f55ff72efbd8e788c388c0f3
Rozan Alawar <rosanalawer2002@gmail.com>
2025-02-15 23:41:59 +0200

    refactor patient login

--- lib/app/modules/auth/views/patient_login_view.dart
+++ lib/app/modules/auth/views/patient_login_view.dart
@@ -11,113 +11,153 @@ import 'package:med_support_gaza/app/routes/app_pages.dart';
 import '../controllers/auth_controller.dart';
 
 class PatientLoginView extends GetView<AuthController> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
   PatientLoginView({super.key});
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20.w,
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: Stack(
               children: [
-                200.height,
-                CustomText(
-                  'Login'.tr,
-                  fontSize: 20.sp,
-                  color: AppColors.accent,
-                  fontWeight: FontWeight.bold,
-                ),
-                64.height,
-                CustomTextField(
-                  hintText: 'Email'.tr,
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) => value!.isValidEmail,
-                ),
-                20.height,
-                Obx(
-                  () => CustomTextField(
-                    hintText: 'Password'.tr,
-                    controller: passwordController,
-                    obscureText: controller.isPasswordVisible.value,
-                    validator: (value) => value!.isValidPassword,
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        size: 18.h,
-                        controller.isPasswordVisible.value
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined,
-                      ),
-                      onPressed: () => controller.togglePasswordVisibility(),
-                    ),
-                  ),
-                ),
-                12.height,
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: GestureDetector(
-                    onTap: () {
-                      Get.toNamed(Routes.FORGET_PASSWORD);
-                    },
-                    child: CustomText(
-                      'ForgotPassword'.tr,
-                      color: AppColors.primary,
-                      fontSize: 11.sp,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                60.height,
-                Obx(
-                  () => CustomButton(
-                    text: 'Login'.tr,
-                    width: double.infinity,
-                    color:controller.isLoading.value?AppColors.textLight: AppColors.primary,
-                    isDisable: controller.isLoading.value,
-                    onPressed: () {
-
-                      if (_formKey.currentState!.validate()) {
-                        print('User Logged In');
-                        controller.signIn(
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim());
-                      }
-                    },
-                  ),
-                ),
-                24.height,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomText(
-                      fontSize: 11.sp,
-                      'DontHaveAccount'.tr,
-                    ),
-                    GestureDetector(
-                      onTap: controller.toggleView,
-                      child: CustomText(
-                        fontSize: 11.sp,
-                        'CreateAccountNow'.tr,
-                        color: AppColors.accent,
-                      ),
-                    ),
-                  ],
-                ),
+                _buildLoginForm(),
+                _buildLoadingIndicator(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        200.height,
+        _buildTitle(),
+        64.height,
+        _buildEmailField(),
+        20.height,
+        _buildPasswordField(),
+        12.height,
+        _buildForgotPasswordButton(),
+        60.height,
+        _buildLoginButton(),
+        24.height,
+        _buildSignUpRow(),
+      ],
+    );
+  }
+
+  Widget _buildTitle() {
+    return CustomText(
+      'Login'.tr,
+      fontSize: 20.sp,
+      color: AppColors.accent,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  Widget _buildEmailField() {
+    return CustomTextField(
+      hintText: 'Email'.tr,
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) => value!.isValidEmail,
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return Obx(
+      () => CustomTextField(
+        hintText: 'Password'.tr,
+        controller: _passwordController,
+        obscureText: controller.isPasswordVisible.value,
+        validator: (value) => value!.isValidPassword,
+        suffixIcon: IconButton(
+          icon: Icon(
+            controller.isPasswordVisible.value
+                ? Icons.visibility_outlined
+                : Icons.visibility_off_outlined,
+            size: 18.h,
+          ),
+          onPressed: controller.togglePasswordVisibility,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForgotPasswordButton() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: GestureDetector(
+        onTap: () => Get.toNamed(Routes.FORGET_PASSWORD),
+        child: CustomText(
+          'ForgotPassword'.tr,
+          color: AppColors.primary,
+          fontSize: 11.sp,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Obx(
+      () => CustomButton(
+        text: 'Login'.tr,
+        width: double.infinity,
+        color: controller.isLoading.value 
+            ? AppColors.textLight 
+            : AppColors.primary,
+        isDisable: controller.isLoading.value,
+        onPressed: _handleLogin,
+      ),
+    );
+  }
+
+  void _handleLogin() {
+    if (_formKey.currentState!.validate()) {
+      controller.signIn(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+    }
+  }
+
+  Widget _buildSignUpRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomText(
+          fontSize: 11.sp,
+          'DontHaveAccount'.tr,
+        ),
+        GestureDetector(
+          onTap: controller.toggleView,
+          child: CustomText(
+            fontSize: 11.sp,
+            'CreateAccountNow'.tr,
+            color: AppColors.accent,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return Obx(
+      () => controller.isLoading.value
+          ? const Center(child: CircularProgressIndicator())
+          : const SizedBox.shrink(),
     );
   }
 }
