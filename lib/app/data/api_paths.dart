139e151071e6763186029580aa70c04f1302700c
Rozan Alawar <rosanalawer2002@gmail.com>
2025-02-18 15:02:08 +0200

    update patient api service

--- lib/app/data/api_paths.dart
+++ lib/app/data/api_paths.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: non_constant_identifier_names
+
 class Links {
   static var baseLink =
       'http://medsupport-gaza-cfd5c72a1744.herokuapp.com/api/';
-  static var login = '/patient/login';
-  static var register = 'register';
-  static var logout = 'logout';
+  static var PATIENT_LOGIN = '/patient/login';
+  static var PATIENT_REGISTER = 'register';
+  static var PATIENT_LOGOUT = 'logout';
   static var doctorRegister = 'doctor/register';
   static var doctorLogin = 'doctor/login';
   static var getDoctorSpecialties = 'doctor/specialties';
--- lib/app/data/api_services/patient_auth_api.dart
+++ lib/app/data/api_services/patient_auth_api.dart
@@ -1,26 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:med_support_gaza/app/data/api_paths.dart';
+import 'package:med_support_gaza/app/data/network_helper/api_exception.dart';
 import 'package:med_support_gaza/app/data/network_helper/dio_helper.dart';
-
-import '../network_helper/api_exception.dart';
 
 class PatientAuthAPIService {
 //------------------------ SIGN IN -----------------------------
 
   static void signIn({
-    required String phoneCode,
-    required String phoneNumber,
-    // required RxBool loading,
+    required String email,
+    required String password,
     required dynamic Function(Response<dynamic>) onSuccess,
     dynamic Function(ApiException)? onError,
     Function? onLoading,
   }) {
     DioHelper.post(
       Links.PATIENT_LOGIN,
-      data: {
-        'phone_code': phoneCode,
-        'phone_number': phoneNumber,
-      },
+      data: {'email': email, 'password': password},
       onSuccess: onSuccess,
       onError: onError,
       onLoading: onLoading,
@@ -30,27 +25,33 @@ class PatientAuthAPIService {
 //------------------------ SIGN UP -----------------------------
 
   static void signUp({
+    required String username,
+    required String email,
+    required String password,
+    required String passwordConfirmation,
     required String firstName,
     required String lastName,
-    required String email,
-    required String dob,
+    required int age,
     required String gender,
     required String phoneNumber,
-    required String phoneCode,
+    required String address,
     required dynamic Function(Response<dynamic>) onSuccess,
-    dynamic Function(ApiException)? onError,
+    Function(ApiException)? onError,
     Function? onLoading,
   }) {
     DioHelper.post(
       Links.PATIENT_REGISTER,
       data: {
+        'username': username,
+        'email': email,
+        'password': password,
+        'password_confirmation': passwordConfirmation,
         'first_name': firstName,
         'last_name': lastName,
-        'email': email,
-        'dob': dob,
+        'age': age,
         'gender': gender,
         'phone_number': phoneNumber,
-        'phone_code': phoneCode,
+        'address': address,
       },
       onSuccess: onSuccess,
       onError: onError,
